f12b0f6812e8b3b0155de813b969ca8752966160
Sergio Martinez <SERGIO.MARTINEZC@OUTLOOK.COM>
2025-07-27 19:14:43 -0600

    Create main.tf

---
+++ IaC/main.tf
@@ -0,0 +1,12 @@
+# main.tf
+
+# Configuración del proveedor de AWS
+provider "aws" {
+  region = "us-east-1" # O la región de tu preferencia
+}
+
+# Definición del bucket S3
+# Yor escaneará este bloque y le añadirá las etiquetas
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "bucket-yor-traced" # El nombre que especificaste
+}
